83fdf962146da2b47d7d0f5b62bc80a424449a7b
Jesper Lundberg <lundberg.jesper@gmail.com>
2023-01-15 22:07:41 +0100

    Set nvim as default editor

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Settings for theme
 set -g theme_use_abbreviated_branch_name yes
 set -g theme_date_format "+%H:%M"
 set -g theme_display_git_branch no
+
+# Set environment variable
+set -gx EDITOR nvim
