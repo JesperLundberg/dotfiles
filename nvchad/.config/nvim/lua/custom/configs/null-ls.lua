6ca489516eb0f999578d949603ebdaea04090d7e
Jesper Lundberg <lundberg.jesper@gmail.com>
2023-04-07 23:39:58 +0200

    Set up code formatter

---
+++ nvchad/.config/nvim/lua/custom/configs/null-ls.lua
@@ -0,0 +1,16 @@
+local null_ls = require "null-ls"
+
+local formatting = null_ls.builtins.formatting
+local lint = null_ls.builtins.diagnostics
+
+local sources = {
+   formatting.csharpier,
+   formatting.stylua,
+
+   lint.shellcheck,
+}
+
+null_ls.setup {
+   debug = true,
+   sources = sources,
+}
