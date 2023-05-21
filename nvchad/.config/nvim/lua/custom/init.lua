cd3392136a636540d25b895b502fe2e2e8ad6eab
Jesper Lundberg <lundberg.jesper@gmail.com>
2023-05-21 20:40:03 +0200

    Set highlighted yank

--- nvchad/.config/nvim/lua/custom/init.lua
+++ nvchad/.config/nvim/lua/custom/init.lua
@@ -1,3 +1,14 @@
 local opt = vim.opt
+local api = vim.api
 
 opt.relativenumber = true
+
+local highlight_group = api.nvim_create_augroup("YankHighlight", { clear = true })
+
+api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = "*",
+})
