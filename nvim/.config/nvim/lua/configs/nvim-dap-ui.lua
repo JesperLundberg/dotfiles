cff99004cac4dd9ac137f16069299f8d6cfd03af
Jesper Lundberg <lundberg.jesper@gmail.com>
2024-03-19 15:00:33 +0100

    replace old nerdfont icons

--- nvim/.config/nvim/lua/configs/nvim-dap-ui.lua
+++ nvim/.config/nvim/lua/configs/nvim-dap-ui.lua
@@ -57,9 +57,9 @@ dapui.setup({
 		icons = {
 			pause = "",
 			play = "",
-			step_into = "",
-			step_over = "",
-			step_out = "",
+			step_into = "󰆹",
+			step_over = "󰆷",
+			step_out = "󰆸",
 			step_back = "",
 			run_last = "↻",
 			terminate = "□",
