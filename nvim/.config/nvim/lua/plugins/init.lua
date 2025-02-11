f08e068211a48477570603e26ed56979f72a6b5d
Jesper Lundberg <jesper.lundberg@alfalaval.com>
2025-02-11 14:35:21 +0100

    Only load dap on LspAttatch event

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -164,7 +164,6 @@ local plugins = {
 		priority = 1000, -- needs to be loaded in first
 		config = function()
 			require("tiny-inline-diagnostic").setup()
-			vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
 		end,
 	},
 	{
@@ -247,7 +246,7 @@ local plugins = {
 		config = function()
 			require("configs.nvim-dap")
 		end,
-		event = "VeryLazy",
+		event = "LspAttach",
 	},
 	{
 		-- UI for debugging
