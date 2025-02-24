3592a994003f07481f8e41aa815154fdc209548c
Jesper Lundberg <jesper.lundberg@alfalaval.com>
2025-02-24 10:13:11 +0100

    Set virtual text to false as it overlaps tiny inline diagnostic plugin

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -163,6 +163,7 @@ local plugins = {
 		event = "LspAttach",
 		priority = 1000, -- needs to be loaded in first
 		config = function()
+			vim.diagnostic.config({ virtual_text = false })
 			require("tiny-inline-diagnostic").setup()
 		end,
 	},
