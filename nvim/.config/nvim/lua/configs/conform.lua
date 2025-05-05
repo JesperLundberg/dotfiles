e3e2a2939cc10df48c39a4afce666f2c461e04a1
Jesper Lundberg <jesper.lundberg@alfalaval.com>
2025-05-05 15:57:58 +0200

    Optimize csharpier, no need to check versions as I AM using 1.x

--- nvim/.config/nvim/lua/configs/conform.lua
+++ nvim/.config/nvim/lua/configs/conform.lua
@@ -11,18 +11,12 @@ M.opts = {
 
 			local command = useDotnet and "dotnet csharpier" or "csharpier"
 
-			local version_out = vim.fn.system(command .. " --version")
-
-			--NOTE: system command returns the command as the first line of the result, need to get the version number on the final line
-			local major_version = tonumber((version_out or ""):match("^(%d+)")) or 0
-			local is_new = major_version >= 1
-
-			local args = is_new and { "format", "$FILENAME" } or { "--write-stdout" }
+			local args = { "format", "$FILENAME" } or { "--write-stdout" }
 
 			return {
 				command = command,
 				args = args,
-				stdin = not is_new,
+				stdin = false,
 				require_cwd = false,
 			}
 		end,
