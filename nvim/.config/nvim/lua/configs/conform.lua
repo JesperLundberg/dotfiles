7caa769bcf726ac0e8c652c702ea6ac3540dc2e2
Jesper Lundberg <jesper.lundberg@alfalaval.com>
2025-05-05 15:54:52 +0200

    Hack to make csharpier version 1+ work properly

--- nvim/.config/nvim/lua/configs/conform.lua
+++ nvim/.config/nvim/lua/configs/conform.lua
@@ -6,6 +6,26 @@ M.opts = {
 		lsp_fallback = true,
 	},
 	formatters = {
+		csharpier = function()
+			local useDotnet = not vim.fn.executable("csharpier")
+
+			local command = useDotnet and "dotnet csharpier" or "csharpier"
+
+			local version_out = vim.fn.system(command .. " --version")
+
+			--NOTE: system command returns the command as the first line of the result, need to get the version number on the final line
+			local major_version = tonumber((version_out or ""):match("^(%d+)")) or 0
+			local is_new = major_version >= 1
+
+			local args = is_new and { "format", "$FILENAME" } or { "--write-stdout" }
+
+			return {
+				command = command,
+				args = args,
+				stdin = not is_new,
+				require_cwd = false,
+			}
+		end,
 		xmlformat = {
 			cmd = { "xmlformat" },
 			args = { "--selfclose", "-" },
