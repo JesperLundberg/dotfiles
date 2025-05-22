c0c311a155fb1fc8d53e0af1fb27d84af1f4a86e
Jesper Lundberg <jesper.lundberg@alfalaval.com>
2025-05-22 09:21:21 +0200

    Make sure diagnostics are shown, virtual text does not need to be disabled, update plugins

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -171,7 +171,6 @@ local plugins = {
 		event = "LspAttach",
 		priority = 1000, -- needs to be loaded in first
 		config = function()
-			vim.diagnostic.config({ virtual_text = false })
 			require("tiny-inline-diagnostic").setup()
 		end,
 	},
