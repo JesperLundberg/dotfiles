3f101f7e142f137caebe157fba4807f1f83d130e
Jesper Lundberg <jesper.lundberg@alfalaval.com>
2025-08-14 09:19:33 +0200

    Set keymap for code actions

--- jesper-nvim/.config/nvim/lua/plugins/tiny-code-action.lua
+++ jesper-nvim/.config/nvim/lua/plugins/tiny-code-action.lua
@@ -1,14 +1,19 @@
 return {
-  {
-    -- Code actions
-    "rachartier/tiny-code-action.nvim",
-    dependencies = {
-      { "nvim-lua/plenary.nvim" },
-      { "nvim-telescope/telescope.nvim" },
-    },
-    event = "LspAttach",
-    config = function()
-      require("tiny-code-action").setup()
-    end,
-  },
+	{
+		-- Code actions
+		"rachartier/tiny-code-action.nvim",
+		dependencies = {
+			{ "nvim-lua/plenary.nvim" },
+			{ "nvim-telescope/telescope.nvim" },
+		},
+		event = "LspAttach",
+		keys = {
+			vim.keymap.set({ "n", "x" }, "<leader>ca", function()
+				require("tiny-code-action").code_action()
+			end, { noremap = true, silent = true }),
+		},
+		config = function()
+			require("tiny-code-action").setup({})
+		end,
+	},
 }
