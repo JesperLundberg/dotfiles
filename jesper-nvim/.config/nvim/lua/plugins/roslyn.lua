bbaa4e89618c9257ac866c78f6ca3daf4e9c7886
Jesper Lundberg <jesper.lundberg@alfalaval.com>
2025-08-15 16:43:02 +0200

    More attempt

--- jesper-nvim/.config/nvim/lua/plugins/roslyn.lua
+++ jesper-nvim/.config/nvim/lua/plugins/roslyn.lua
@@ -3,8 +3,10 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	opts = {
 		broad_search = true,
-		ignore_target = function(sln)
-			return string.match(sln, "SmartDok.sln") ~= nil
+		root_dir = function(fname)
+			return require("lspconfig.util").root_pattern("*.sln")(fname)
+				or require("lspconfig.util").root_pattern("*.csproj")(fname)
+				or require("lspconfig.util").root_pattern(".git")(fname)
 		end,
 	},
 	-- "seblyng/roslyn.nvim",
