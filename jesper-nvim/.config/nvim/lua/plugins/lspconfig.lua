a1fb668ffb7ba7762f825d44b19d57d9c571a972
Jesper Lundberg <jesper.lundberg@alfalaval.com>
2025-08-19 15:19:13 +0200

    Move functionality so it doesn't get overwritten

--- jesper-nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ jesper-nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -39,104 +39,31 @@ return {
 						vim.notify("LspAttach for " .. vim.api.nvim_buf_get_name(event.buf))
 					end
 
-					vim.keymap.set("n", "<leader>ra", vim.lsp.buf.rename, { desc = "LSP: Rename" })
-					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: Code Action" })
+					local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
+					vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+						buffer = event.buf,
+						group = highlight_augroup,
+						callback = vim.lsp.buf.document_highlight,
+					})
 
-					-- Rename the variable under your cursor.
-					--  Most Language Servers support renaming across files, etc.
-					-- map("<leader>ra", vim.lsp.buf.rename, "[R]e[n]ame")
-					--
-					-- -- Execute a code action, usually your cursor needs to be on top of an error
-					-- -- or a suggestion from your LSP for this to activate.
-					-- map("<leader>ca", vim.lsp.buf.code_action, "Goto [C]ode [A]ction", { "n", "x" })
-					--
-					-- -- Find references for the word under your cursor.
-					-- map("<leader>gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-					--
-					-- -- Jump to the implementation of the word under your cursor.
-					-- --  Useful when your language has ways of declaring types without an actual implementation.
-					-- map("<leader>gi", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-					--
-					-- -- Jump to the definition of the word under your cursor.
-					-- --  This is where a variable was first declared, or where a function is defined, etc.
-					-- --  To jump back, press <C-t>.
-					-- map("<leader>gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-					--
-					-- -- WARN: This is not Goto Definition, this is Goto Declaration.
-					-- --  For example, in C this would take you to the header.
-					-- map("<leader>gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-					--
-					-- -- Fuzzy find all the symbols in your current document.
-					-- --  Symbols are things like variables, functions, types, etc.
-					-- map("gO", require("telescope.builtin").lsp_document_symbols, "Open Document Symbols")
-					--
-					-- -- Fuzzy find all the symbols in your current workspace.
-					-- --  Similar to document symbols, except searches over your entire project.
-					-- map("gW", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Open Workspace Symbols")
-					--
-					-- -- Jump to the type of the word under your cursor.
-					-- --  Useful when you're not sure what type a variable is and you want to see
-					-- --  the definition of its *type*, not where it was *defined*.
-					-- map("grt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
+					vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+						buffer = event.buf,
+						group = highlight_augroup,
+						callback = vim.lsp.buf.clear_references,
+					})
 
-					-- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
-					---@param client vim.lsp.Client
-					---@param method vim.lsp.protocol.Method
-					---@param bufnr? integer some lsp support methods only in specific files
-					---@return boolean
-					local function client_supports_method(client, method, bufnr)
-						if vim.fn.has("nvim-0.11") == 1 then
-							return client:supports_method(method, bufnr)
-						else
-							return client.supports_method(method, { bufnr = bufnr })
-						end
-					end
-
-					-- The following two autocommands are used to highlight references of the
-					-- word under your cursor when your cursor rests there for a little while.
-					--    See `:help CursorHold` for information about when this is executed
-					--
-					-- When you move your cursor, the highlights will be cleared (the second autocommand).
-					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if
-						client
-						and client_supports_method(
-							client,
-							vim.lsp.protocol.Methods.textDocument_documentHighlight,
-							event.buf
-						)
-					then
-						local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
-						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-							buffer = event.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.document_highlight,
-						})
-
-						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-							buffer = event.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.clear_references,
-						})
-
-						vim.api.nvim_create_autocmd("LspDetach", {
-							group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
-							callback = function(event2)
-								vim.lsp.buf.clear_references()
-								vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event2.buf })
-							end,
-						})
-					end
+					vim.api.nvim_create_autocmd("LspDetach", {
+						group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
+						callback = function(event2)
+							vim.lsp.buf.clear_references()
+							vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event2.buf })
+						end,
+					})
 
 					-- toggle inlay hints
-					if
-						client
-						and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf)
-					then
-						map("<leader>th", function()
-							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
-						end, "[T]oggle Inlay [H]ints")
-					end
+					map("<leader>th", function()
+						vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+					end, "[T]oggle Inlay [H]ints")
 				end,
 			})
 
