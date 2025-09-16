4218ff91d25842baa99d3a94ea1b36b9cdaa137c
Jesper Lundberg <jesper.lundberg@alfalaval.com>
2025-09-16 15:11:09 +0200

    telescope old files respects cwd and only shows old files in cwd

--- jesper-nvim/.config/nvim/lua/plugins/telescope.lua
+++ jesper-nvim/.config/nvim/lua/plugins/telescope.lua
@@ -22,7 +22,11 @@ return {
 					},
 					sorting_strategy = "ascending",
 				},
-				-- pickers = {}
+				pickers = {
+					oldfiles = {
+						only_cwd = true,
+					},
+				},
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown(),
