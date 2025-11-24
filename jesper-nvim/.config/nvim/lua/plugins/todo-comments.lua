f64d9c5acf7fc0eae1d50b708016b9d3c33cafa8
Jesper Lundberg <jesper.lundberg@alfalaval.com>
2025-11-24 14:15:51 +0100

    Make vim lighter to be able to run on rpi zero 2

--- jesper-nvim/.config/nvim/lua/plugins/todo-comments.lua
+++
@@ -1,8 +0,0 @@
-return {
-	{
-		"folke/todo-comments.nvim",
-		event = "VimEnter",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {},
-	},
-}
--- jesper-nvim/.config/nvim/lua/plugins/treesitter.lua
+++ jesper-nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -16,7 +16,7 @@ return {
 				"vimdoc",
 			},
 			-- Autoinstall languages that are not installed
-			auto_install = true,
+			auto_install = false,
 			highlight = {
 				enable = true,
 				additional_vim_regex_highlighting = { "ruby" },
