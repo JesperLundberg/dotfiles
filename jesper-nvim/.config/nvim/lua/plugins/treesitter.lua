eec2dd5b954a3cb9e05ffd2460e0f17e450282ea
Jesper Lundberg <lundberg.jesper@gmail.com>
2025-12-30 23:15:36 +0100

    Fix treesitter

--- jesper-nvim/.config/nvim/lua/plugins/treesitter.lua
+++ jesper-nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
 	{ -- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		main = "nvim-treesitter.configs", -- Sets main module to use for opts
+		main = "nvim-treesitter.config", -- Sets main module to use for opts
 		opts = {
 			ensure_installed = {
 				"bash",
