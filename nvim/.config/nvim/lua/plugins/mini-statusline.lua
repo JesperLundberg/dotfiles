f08786e310c14ebda03e36d8d0044dc50160e1e5
Jesper Lundberg <jesper.lundberg@alfalaval.com>
2026-09-22 10:40:23 +0200

    Change how statusline is truncated

--- nvim/.config/nvim/lua/plugins/mini-statusline.lua
+++ nvim/.config/nvim/lua/plugins/mini-statusline.lua
@@ -15,7 +15,12 @@ end
 
 local function cwd_relative_path()
 	local fname = vim.api.nvim_buf_get_name(0)
-	return fname ~= "" and vim.fn.fnamemodify(fname, ":.") or ""
+
+	if fname == "" then
+		return ""
+	end
+
+	return vim.fn.fnamemodify(fname, ":.")
 end
 
 local function diagnostic_groups()
@@ -30,9 +35,13 @@ local function diagnostic_groups()
 	end
 
 	local groups = {}
+
 	local function push(count, icon, hl)
 		if count > 0 then
-			table.insert(groups, { hl = hl, strings = { ("%s %d"):format(icon, count) } })
+			table.insert(groups, {
+				hl = hl,
+				strings = { ("%s %d"):format(icon, count) },
+			})
 		end
 	end
 
@@ -46,30 +55,41 @@ end
 
 local function get_lsps()
 	local clients = {}
+
 	for _, client in pairs(vim.lsp.get_clients({ bufnr = 0 })) do
 		table.insert(clients, client.name)
 	end
+
 	local ok, conform = pcall(require, "conform")
+
 	if ok then
 		for _, f in pairs(conform.list_formatters(0)) do
 			add_only_unique(clients, f.name)
 		end
 	end
+
 	if #clients == 0 then
 		return ""
 	end
-	return (vim.o.columns > 100 and (table.concat(clients, ", ") .. " ")) or "  LSP "
+
+	return vim.o.columns > 100 and (table.concat(clients, ", ") .. " ") or "  LSP "
 end
 
 function M.setup()
 	local statusline = require("mini.statusline")
+
 	statusline.setup({
 		use_icons = vim.g.have_nerd_font,
+
 		content = {
 			active = function()
 				local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
-				local groups = {
-					{ hl = mode_hl, strings = { mode } },
+
+				local left_groups = {
+					{
+						hl = mode_hl,
+						strings = { mode },
+					},
 					{
 						hl = "MiniStatuslineDevinfo",
 						strings = {
@@ -77,15 +97,41 @@ function M.setup()
 							statusline.section_diff({ trunc_width = 75 }),
 						},
 					},
-					{ hl = "MiniStatuslineFilename", strings = { cwd_relative_path() } },
-					"%=",
 				}
-				vim.list_extend(groups, diagnostic_groups())
-				vim.list_extend(groups, {
-					{ hl = "MiniStatuslineDevinfo", strings = { get_lsps() } },
-					{ hl = "MiniStatuslineFilename", strings = { vim.bo.filetype } },
-					{ hl = mode_hl, strings = { "%l/%-L (%p%%)" } },
+
+				local right_groups = diagnostic_groups()
+
+				vim.list_extend(right_groups, {
+					{
+						hl = "MiniStatuslineDevinfo",
+						strings = { get_lsps() },
+					},
+					{
+						hl = "MiniStatuslineFilename",
+						strings = { vim.bo.filetype },
+					},
+					{
+						hl = mode_hl,
+						strings = { "%l/%-L (%p%%)" },
+					},
 				})
+
+				local groups = {}
+
+				vim.list_extend(groups, left_groups)
+
+				-- This is the point where Vim is allowed to truncate.
+				table.insert(groups, "%<")
+
+				table.insert(groups, {
+					hl = "MiniStatuslineFilename",
+					strings = { cwd_relative_path() },
+				})
+
+				table.insert(groups, "%=")
+
+				vim.list_extend(groups, right_groups)
+
 				return statusline.combine_groups(groups)
 			end,
 		},
